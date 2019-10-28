d4f963c87579f613b0aae63cc8aae4c7c81b6c08
User Name <yoroshikudouzobbb@gmail.com>
2019-10-28 10:03:27 +0900

    users.showページの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_27_225834) do
+ActiveRecord::Schema.define(version: 2019_10_28_005826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_10_27_225834) do
     t.string "user_name"
     t.string "website"
     t.text "bio"
+    t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
