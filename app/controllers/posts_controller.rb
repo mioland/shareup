c1e8bddc33165dcff80af81592e06ccef6609218
User Name <yoroshikudouzobbb@gmail.com>
2019-10-29 09:29:25 +0900

    relationshipモデルの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,16 +14,23 @@ class PostsController < ApplicationController
       @post = Post.new(post_params)
       if params[:images]
         params[:images].each do |img|
+          binding.pry
           @post.save
-          @post.photos.create(image: img)
+          @photos = @post.photos.new(image: img)
+        end
+        if @photos.save
           redirect_to root_path
           flash[:notice] = "投稿が保存されました"
+        else
+          redirect_to new_post_path
+          flash[:notice] = "画像が保存できません"
         end
       else
         redirect_to root_path
         flash[:alert] = "投稿に失敗しました"
       end
     end
+  
   
     def show
       @photos = @post.photos
