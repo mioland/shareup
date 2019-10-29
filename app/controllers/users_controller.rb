3acf21e1bd26df74c2df151b3703b2f6857943d9
User Name <yoroshikudouzobbb@gmail.com>
2019-10-29 14:53:34 +0900

    フォロー/フォロワーの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
     def index
-        @users = User.search(params[:search])
-      end
-    
+      @users = User.search(params[:search])
+    end
     def show
       @user = User.find(params[:id])
     end
-  end
+  
+    def following
+      @user = User.find(params[:id])
+    end
+  
+    def followers
+      @user = User.find(params[:id])
+    end
+end
