d756c421daed0c3a0b2de5174cb77958e08eb044
User Name <yoroshikudouzobbb@gmail.com>
2019-10-29 14:39:44 +0900

    いいねした投稿一覧ページを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,14 @@ class User < ApplicationRecord
     user
   end
 
+  def self.search(search) #ここでのself.はUser.を意味する
+    if search
+     where(['name LIKE ?', "%#{search}%"]) #検索とnameの部分一致を表示。User.は省略
+    else
+     all #全て表示。User.は省略
+    end
+  end
+
   def feed
     Post.where("user_id IN (:following_ids)", following_ids: following_ids)
   end
