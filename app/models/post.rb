6940ca48868c395b217a6e33f48cebf17687ef18
User Name <yoroshikudouzobbb@gmail.com>
2019-10-29 11:04:16 +0900

    commentモデル、コントローラの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
     has_many :photos, dependent: :destroy
     acts_as_taggable
     has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+    has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
 
     def liked_by(user)
       Like.find_by(user_id: user.id, post_id: id)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_013804) do
+ActiveRecord::Schema.define(version: 2019_10_29_015114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment", null: false
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.bigint "post_id", null: false
@@ -95,6 +105,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_013804) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "photos", "posts"
