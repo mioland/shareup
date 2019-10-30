c201ebc2ebca5243af3c6ba871c0ca725bb6691a
User Name <yoroshikudouzobbb@gmail.com>
2019-10-31 08:14:07 +0900

    新規投稿ページのデザイン変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,61 +1,61 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!, except: %i(index)
-    before_action :set_post, only: %i(show destroy)
+  before_action :authenticate_user!, except: %i(index)
+  before_action :set_post, only: %i(show destroy)
 
-    def index
-      if user_signed_in?
-        @posts = current_user.feed.limit(10).includes(:photos, :user, :likes).order('created_at DESC')
-      else
-        redirect_to home_path
+  def index
+    if user_signed_in?
+      @posts = current_user.feed.includes(:photos, :user, :likes).order("created_at DESC").page(params[:page]).per(POST_PER)
+    else
+      redirect_to home_path
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:images]
+      params[:images].each do |img|
+        binding.pry
+        @post.save
+        @photos = @post.photos.new(image: img)
       end
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
-      @post = Post.new(post_params)
-      if params[:images]
-        params[:images].each do |img|
-          binding.pry
-          @post.save
-          @photos = @post.photos.new(image: img)
-        end
-        if @photos.save
-          redirect_to root_path
-          flash[:notice] = "投稿が保存されました"
-        else
-          redirect_to new_post_path
-          flash[:notice] = "画像が保存できません"
-        end
-      else
+      if @photos.save
         redirect_to root_path
-        flash[:alert] = "投稿に失敗しました"
-      end
-    end
-  
-  
-    def show
-      @photos = @post.photos
-      @likes = @post.likes.includes(:user)
-    end
-  
-    def destroy
-      if @post.user == current_user
-        flash[:notice] = "投稿が削除されました" if @post.destroy
+        flash[:notice] = "投稿が保存されました"
       else
-        flash[:alert] = "投稿の削除に失敗しました"
+        redirect_to new_post_path
+        flash[:notice] = "画像が保存できません"
       end
+    else
       redirect_to root_path
+      flash[:alert] = "投稿に失敗しました"
     end
+  end
 
-    private
-      def post_params
-        params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
-      end
+  def show
+    @photos = @post.photos
+    @likes = @post.likes.includes(:user)
+  end
 
-      def set_post
-        @post = Post.find_by(id: params[:id])
-      end
+  def destroy
+    if @post.user == current_user
+      flash[:notice] = "投稿が削除されました" if @post.destroy
+    else
+      flash[:alert] = "投稿の削除に失敗しました"
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
+  end
 end
