2f4a86659a6a9df7c395946a8e2e4ff4161ee234
User Name <yoroshikudouzobbb@gmail.com>
2019-10-30 19:48:22 +0900

    uploaderの設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Shareup
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.autoload_paths += Dir[Rails.root.join("app", "uploaders")]
 
     config.i18n.default_locale = :ja
     config.action_view.embed_authenticity_token_in_remote_forms = true
