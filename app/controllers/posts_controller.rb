40c88dab616bf287645b333db9ae876337006dab
User Name <yoroshikudouzobbb@gmail.com>
2019-12-13 20:42:51 +0900

    circleciテスト

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,17 +18,11 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     if params[:images]
       params[:images].each do |img|
-        binding.pry
         @post.save
-        @photos = @post.photos.new(image: img)
+        @post.photos.create(image: img)
       end
-      if @photos.save
-        redirect_to root_path
-        flash[:notice] = "投稿が保存されました"
-      else
-        redirect_to new_post_path
-        flash[:notice] = "画像が保存できません"
-      end
+      redirect_to root_path
+      flash[:notice] = "投稿が保存されました"
     else
       redirect_to root_path
       flash[:alert] = "投稿に失敗しました"
@@ -52,7 +46,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :image,).merge(user_id: current_user.id)
+    params.require(:post).permit(:caption).merge(user_id: current_user.id)
   end
 
   def set_post
